d2654a2e1d5bab2fd8aedc40814febea83cceec7
RadoslawZemlo <radoslawzemlo1@gmail.com>
2022-03-06 23:25:36 +0100

    Add image and icon change on arrow click

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import img0 from "../public/img/jutro-img-0.png";
+import img1 from "../public/img/jutro-img-1.png";
+import img2 from "../public/img/jutro-img-2.png";
 import icon0 from "../public/icon/icon-0.png";
+import icon1 from "../public/icon/icon-1.png";
+import icon2 from "../public/icon/icon-2.png";
 
 export default function Home() {
   const [screen, setScreen] = useState(0);
+  const [imgSrc, setImgSrc] = useState(img0);
+  const [icon, setIcon] = useState(icon0);
+
+  useEffect(() => {
+    if (screen === 0) {
+      setImgSrc(img0);
+      setIcon(icon0);
+    } else if (screen === 1) {
+      setImgSrc(img1);
+      setIcon(icon1);
+    } else if (screen === 2) {
+      setImgSrc(img2);
+      setIcon(icon2);
+    }
+  }, [screen]);
 
   const handleLeft = () => {
     if (screen === 0) {
@@ -22,15 +41,18 @@ export default function Home() {
     }
   };
 
-  console.log(screen);
-
   return (
     <div
       className={`w-screen h-screen text-white flex items-center transition ease-in-out duration-1500 ${
         screen === 0 && "bg-first"
       } ${screen === 1 && "bg-second"} ${screen === 2 && "bg-third"}`}
     >
-      <div className="p-2 cursor-pointer" onClick={handleLeft}>
+      <div
+        className={`p-2 cursor-pointer transition ease-in-out duration-1500 ${
+          screen === 2 && "text-black"
+        }`}
+        onClick={handleLeft}
+      >
         &lt;
       </div>
 
@@ -41,11 +63,23 @@ export default function Home() {
           }`}
         >
           <div className="mt-10">
-            <Image src={icon0} alt="icon-0" width={120} height={120} />
+            <Image src={icon} alt="icon-0" width={120} height={120} />
           </div>
-          <h1 className="text-2xl font-bold py-5">Pobierz aplikację</h1>
+          <h1
+            className={`text-2xl font-bold py-5 transition ease-in-out duration-1500 ${
+              screen === 2 && "text-black"
+            }`}
+          >
+            {screen === 0 && "Pobierz aplikację"}
+            {screen === 1 && "Porozmawiaj z lekarzem"}
+            {screen === 2 && "Otrzymaj receptę"}
+          </h1>
 
-          <p className="text-sm mb-6">
+          <p
+            className={`text-sm mb-6 transition ease-in-out duration-1500 ${
+              screen === 2 && "text-black"
+            }`}
+          >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
             turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
             nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
@@ -65,11 +99,16 @@ export default function Home() {
             screen === 1 ? "translate-y-[-900px] lg:translate-y-[-550px]" : ""
           }`}
         >
-          <Image src={img0} alt="img" />
+          <Image src={imgSrc} alt="img" />
         </div>
       </div>
 
-      <div className="p-2 cursor-pointer" onClick={handleRight}>
+      <div
+        className={`p-2 cursor-pointer lg:px-12 transition ease-in-out duration-1500 ${
+          screen === 2 && "text-black"
+        }`}
+        onClick={handleRight}
+      >
         &gt;
       </div>
     </div>

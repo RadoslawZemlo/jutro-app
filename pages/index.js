2799a2fc61d8ad8db1ea41457653dfa4c5b6a7b7
RadoslawZemlo <radoslawzemlo1@gmail.com>
2022-03-06 22:17:35 +0100

    Build first screen layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import img0 from "../public/img/jutro-img-0.png";
+
 export default function Home() {
   return (
-    <div className="bg-[#26c299] w-screen h-screen text-white">
-      <h1>Pobierz aplikację</h1>
+    <div className="bg-[#26c299] w-screen h-screen text-white flex items-center">
+      <div className="p-2 cursor-pointer">&lt;</div>
+
+      <div className="mx-6 h-full overflow-hidden">
+        <h1 className="text-2xl font-bold py-5">Pobierz aplikację</h1>
+
+        <p className="text-sm mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus.
+        </p>
+        <input
+          className="cursor-pointer py-2 px-4 bg-[#0a40a1]"
+          type="submit"
+          value="Pobierz aplikację"
+        />
+
+        <div className="w-full relative top-28">
+          <Image src={img0} alt="img" />
+        </div>
+      </div>
+
+      <div className={`p-2 cursor-pointer`}>&gt;</div>
     </div>
   );
 }
